362363020948a7b96849e6b8f45f6913e279b2d3
Arkadiusz Gac <arkadiusz.gac@gmail.com>
2015-03-19 20:15:32 +0100

    Add new file and directories

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 require_once __DIR__.'/vendor/autoload.php';
 
-//define('PROCESSED_IMAGES', 'processed_images');
-
 define('PROCESSED_IMAGES', 'processed_images');
 
 define('LAST_IMAGE', 'last_image');
